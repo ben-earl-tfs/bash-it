11381d28bfb8a340f6c019bf06b9054dbde21d24
Ben Earl <ben@shellbox.io>
2020-06-08 08:38:31 -0400

    Added my theme

---
+++ themes/ben/ben.theme.bash
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" ${green}|"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}["
+GIT_THEME_PROMPT_SUFFIX="${green}]"
+
+#RVM_THEME_PROMPT_PREFIX="|"
+#RVM_THEME_PROMPT_SUFFIX="|"
+CONDAENV_THEME_PROMPT_PREFIX="${green}["
+CONDAENV_THEME_PROMPT_SUFFIX="${green}]"
+
+__bobby_clock() {
+  printf "$(clock_prompt) "
+
+  if [ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]; then
+    printf "$(clock_char) "
+  fi
+}
+
+__venv_check() {
+    if [ "$VIRTUAL_ENV" != '' ]; then
+        printf "${reset_color}:${yellow}$(python_version_prompt)"
+    fi
+}
+
+function prompt_command() {
+    #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple} $(python_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color} \n${green} →${reset_color} "
+    PS1="\n$(battery_char) $(__bobby_clock)${purple}\h ${reset_color}in ${green}\w$(__venv_check)${reset_color}  \n ${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+}
+
+THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
+THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
+THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
+THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M:%S"}
+
+safe_append_prompt_command prompt_command
