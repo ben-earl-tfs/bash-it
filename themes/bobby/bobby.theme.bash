94c202424ebfbdbc64cec19f3dc8e83deaa29a4c
Ben Earl <ben@shellbox.io>
2020-06-08 09:21:29 -0400

    Modified bobby theme, mine didn't work

--- themes/bobby/bobby.theme.bash
+++ themes/bobby/bobby.theme.bash
@@ -21,9 +21,17 @@ __bobby_clock() {
   fi
 }
 
+__venv_check() {
+    if [ "$VIRTUAL_ENV" != '' ]; then
+        printf "${reset_color}${yellow}$(python_version_prompt):"
+    fi
+}
+
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+#    PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+     PS1="\n$(battery_char) $(__bobby_clock)${purple}\h ${reset_color}in $(__venv_check)${green}\w${reset_color}  \n ${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+
 }
 
 THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
